35c43f37f8a436ebb43dfdac60a0c4a107ae00b1
Martin K <nitrammok@gmail.com>
2025-10-05 10:27:45 +0300

    task completed

---
+++ blogicum/blog/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render, get_object_or_404
+
+from .models import Post, Category
+
+from django.utils import timezone
+
+from django.http import Http404
+
+
+def index(request):
+    template = 'blog/index.html'
+    post_list = Post.objects.filter(
+        pub_date__lte=timezone.now(),
+        is_published=True,
+        category__is_published=True
+    ).order_by('-pub_date')[:5]
+    context = {'post_list': post_list}
+    return render(request, template, context)
+
+
+def post_detail(request, id):
+    template = 'blog/detail.html'
+    post = get_object_or_404(Post, pk=id)
+    if post.pub_date > timezone.now() or not post.is_published or not post.category.is_published:  # Не разобрался, как перенести эту строку.
+        raise Http404("Публикация не найдена")
+    context = {
+        'post': post
+    }
+    return render(request, template, context)
+
+
+def category_posts(request, category_slug):
+    template = 'blog/category.html'
+    category = get_object_or_404(Category, slug=category_slug)
+    if not category.is_published:
+        raise Http404("Категория не найдена")
+    post_list = Post.objects.filter(
+        category=category,
+        is_published=True,
+        pub_date__lte=timezone.now()
+    ).order_by('-pub_date')
+    context = {'post_list': post_list, 'category_slug': category_slug}
+    return render(request, template, context)
---
+++ blogicum/core/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class PublishedModel(models.Model):
+    """Абстрактная модель. Добвляет флаг is_published."""
+
+    is_published = models.BooleanField(
+        default=True,
+        verbose_name='Опубликовано',
+        help_text='Снимите галочку, чтобы скрыть публикацию.'
+    )
+
+    class Meta:
+        abstract = True
